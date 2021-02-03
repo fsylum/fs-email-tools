a1fd90c142e32b87b320f53cb0bcc3277a12029a
Firdaus Zahari <firdaus@fsylum.net>
2021-02-03 10:00:41 +0800

    Implement send test email functionality

--- src/WP/Admin/Page.php
+++ src/WP/Admin/Page.php
@@ -9,6 +9,7 @@ class Page extends Service
     public function run()
     {
         add_action('admin_menu', [$this, 'addPage']);
+        add_action('admin_post_fs_email_tools_send_test_email', [$this, 'sendTestEmail']);
     }
 
     public function addPage()
@@ -22,16 +23,25 @@ class Page extends Service
             return;
         }
 
-        if (isset($_GET['settings-updated']) && empty(get_settings_errors(Settings::KEY))) {
-            add_settings_error(Settings::KEY, 'fs_email_tools_status', __( 'Settings saved.', 'fs-email-tools' ), 'updated');
-        }
-
-        settings_errors(Settings::KEY);
-
         $all_tabs    = $this->tabs();
         $default_tab = array_keys($all_tabs);
         $default_tab = array_shift($default_tab);
         $current_tab = !empty($_GET['tab']) && in_array($_GET['tab'], array_keys($all_tabs)) ? sanitize_key($_GET['tab']) : $default_tab;
+
+        if (isset($_GET['settings-updated']) && empty(get_settings_errors(Settings::KEY))) {
+            add_settings_error(Settings::KEY, 'fs_email_tools_status', __( 'Settings saved.', 'fs-email-tools' ), 'updated');
+        }
+
+        if ($current_tab === 'test-email' && isset($_GET['sent'])) {
+            if (sanitize_text_field($_GET['sent']) === 'yes') {
+                add_settings_error(Settings::KEY, 'fs_email_tools_status', __( 'Test email is successfully sent.', 'fs-email-tools' ), 'updated');
+            } else {
+                add_settings_error(Settings::KEY, 'fs_email_tools_status', __( 'Failed to send test email.', 'fs-email-tools' ));
+            }
+        }
+
+        settings_errors(Settings::KEY);
+
         ?>
             <div class="wrap">
                 <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
@@ -63,5 +73,25 @@ class Page extends Service
             'page' => $this->plugin::SLUG,
             'tab' => $tab,
         ], admin_url('tools.php'));
+    }
+
+    public function sendTestEmail()
+    {
+        if (!wp_verify_nonce($_REQUEST['_wpnonce'], 'fs-email-tools-test-email')) {
+            wp_die('Invalid request');
+        }
+
+        $to      = sanitize_email($_REQUEST['to']);
+        $subject = sanitize_text_field($_REQUEST['subject']);
+        $message = sanitize_textarea_field($_REQUEST['message']);
+
+        $result = wp_mail($to, $subject, $message);
+
+        wp_redirect(
+            add_query_arg([
+                'sent' => $result ? 'yes' : 'no',
+            ], $this->tabUrl('test-email'))
+        );
+        exit;
     }
 }
--- templates/tabs/test-email.php
+++ templates/tabs/test-email.php
@@ -2,23 +2,25 @@
 
 <form action="<?php echo esc_url(admin_url('admin-post.php')) ?>" method="post">
     <?php wp_nonce_field('fs-email-tools-test-email'); ?>
+    <input type="hidden" name="action" value="fs_email_tools_send_test_email">
 
     <table class="form-table">
         <tbody>
             <tr>
                 <th scope="row"><label for="fs-email-tools-test-to">To</label></th>
-                <td><input name="to" type="text" id="fs-email-tools-test-to" value="<?php echo esc_attr(get_option('admin_email')); ?>" class="regular-text"></td>
+                <td><input name="to" type="text" id="fs-email-tools-test-to" value="<?php echo esc_attr(get_option('admin_email')); ?>" class="regular-text" required></td>
             </tr>
             <tr>
                 <th scope="row"><label for="fs-email-tools-test-subject">Subject</label></th>
-                <td><input name="subject" type="text" id="fs-email-tools-test-subject" value="" class="regular-text"></td>
+                <td><input name="subject" type="text" id="fs-email-tools-test-subject" value="Test email from <?php echo esc_attr(get_bloginfo('name')); ?>" class="regular-text" required></td>
             </tr>
             <tr>
                 <th scope="row"><label for="fs-email-tools-test-message">Message</label></th>
-                <td><textarea message="subject" id="fs-email-tools-test-message" class="regular-text" rows="10"></textarea></td>
+                <td><textarea name="message" id="fs-email-tools-test-message" class="regular-text" rows="10" required>It works!</textarea></td>
             </tr>
         </tbody>
     </table>
+
+    <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Send Test Email"></p>
 </form>
 
-<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Send Test Email"></p>
