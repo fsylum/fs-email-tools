73bbfa4b90a2b5aa8bacd3a716a77ee61b5afb37
Firdaus Zahari <firdaus@fsylum.net>
2021-02-04 22:01:54 +0800

    Fix some interaction

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -4,28 +4,32 @@
             let isChecked = $(this).is(':checked');
 
             $('#fs-email-tools-reroute-recipients, #fs-email-tools-reroute-append-status').prop('disabled', !isChecked);
+            $('#fs-email-tools-reroute-append-status').trigger('change');
         }).change();
 
         $('#fs-email-tools-reroute-append-status').on('change', function () {
-            let isChecked = $(this).is(':checked');
+            let isChecked         = $(this).is(':checked');
+            let isRereouteEnabled = $('#fs-email-tools-reroute-status').is(':checked');
 
-            $('#fs-email-tools-reroute-append-location').prop('disabled', !isChecked);
+            $('#fs-email-tools-reroute-append-location').prop('disabled', (!isChecked || !isRereouteEnabled));
         }).change();
 
         $('#fs-email-tools-log-status').on('change', function () {
             let isChecked = $(this).is(':checked');
 
             $('.fs-email-tools-log-keep-indefinitely').prop('disabled', !isChecked);
+            $('.fs-email-tools-log-keep-indefinitely').trigger('change')
         }).change();
 
         $('.fs-email-tools-log-keep-indefinitely').on('change', function () {
             let checkedValue = $(this).filter(':checked').val();
+            let isLogEnabled = $('#fs-email-tools-log-status').is(':checked');
 
             if (typeof checkedValue === 'undefined') {
                 return;
             }
 
-            $('#fs-email-tools-log-keep-in-days').prop('disabled', checkedValue == 1);
+            $('#fs-email-tools-log-keep-in-days').prop('disabled', (checkedValue == 1 || !isLogEnabled));
         }).change();
     });
 })(jQuery);
